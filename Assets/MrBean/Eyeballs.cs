4333e36828dae3383216561b8f26c5eec5cc0163
DAwesomEL <45754338+DAwesomEL@users.noreply.github.com>
2023-01-11 13:01:26 +0100

    Ymse

--- Assets/MrBean/Eyeballs.cs
+++ Assets/MrBean/Eyeballs.cs
@@ -29,7 +29,7 @@ public class Eyeballs : MonoBehaviour
 
         xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 70f);
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }   
 }
